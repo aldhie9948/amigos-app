652cbc2eed4a4fcdbf5d58c0c930a7c6d4f92b87
Aldi Gunawan <aldigunawan458@gmail.com>
2022-04-06 20:17:27 +0700

    pwa builds

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,33 @@
-import "../styles/globals.css";
-import { Provider } from "react-redux";
-import store from "../store";
+import '../styles/globals.css';
+import { Provider } from 'react-redux';
+import store from '../store';
+import Head from 'next/head';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/sw.js').then(
+          function (registration) {
+            console.log(
+              'Service Worker registration successful with scope: ',
+              registration.scope
+            );
+          },
+          function (err) {
+            console.log('Service Worker registration failed: ', err);
+          }
+        );
+      });
+    }
+  }, []);
+
   return (
     <Provider store={store}>
+      <Head>
+        <link rel='manifest' href='manifest.json' />
+      </Head>
       <Component {...pageProps} />
     </Provider>
   );
